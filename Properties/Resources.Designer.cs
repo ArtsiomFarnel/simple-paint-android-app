28946e17d7476be5cec8b48ae75593b6af4eb77c
Artsiom Farnel <87361967+leeqviz@users.noreply.github.com>
2024-01-14 16:49:06 +0300

    Update Resources.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace SimpleCalculator.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("lab1_.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleCalculator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
